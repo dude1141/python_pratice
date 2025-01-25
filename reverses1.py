2e5520bcc668d31186294584e5f5c6dee7671df2
Mithlesh_1141 <48873398+dude1141@users.noreply.github.com>
2025-01-25 17:59:06 -0500

    Update reverses1.py

--- reverses1.py
+++ reverses1.py
@@ -4,7 +4,7 @@ class reverses1:
     def method2(self):
         a=""
         b=reverses1.a1.split(",") #use split and then reverseit
-        for i in range(len(b)-1,-1,-1):
+        for i in range(len(b)-1,-1,-1):    #(b)-1 points to index 4 (start) ,-1 points to final index 0 value(hello) (Stop),-1 increment
             a=a+" "+b[i]
             print(a)
         print(a)
