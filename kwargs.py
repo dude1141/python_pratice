65b908e30f556682f85f2486b107c0861f7b8abc
Mithelesh_1141 <48873398+dude1141@users.noreply.github.com>
2025-02-10 21:30:35 -0500

    Create kwargs.py

---
+++ kwargs.py
@@ -0,0 +1,11 @@
+def func(**Kwargs):
+	for i,val in Kwargs.items():
+		print("....",(i,val))
+
+func(s1='Geeks',s2='For')
+
+       key    value
+.... ('s1', 'Geeks')
+.... ('s2', 'For')
+
+# arguments are stored as  key-value pair
