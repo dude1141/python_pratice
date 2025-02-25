4fe715df964351b78f9f94de8069a8b2f68cc444
Mithelesh_1141 <48873398+dude1141@users.noreply.github.com>
2025-02-25 12:41:37 -0500

    Create pyramids.py

---
+++ pyramids.py
@@ -0,0 +1,23 @@
+pyramid 
+
+#reversing pyrmaid
+for i in range(5,0,-1):  #(start,stop,decrement)
+    # i=5
+    # print("i...",i)
+    for j in range(i-1):
+        # print(j-1, end=" ")
+        # print("j....",j)
+        print("*",end="")
+    print()
+
+
+#pyramid
+for i in range(5):
+    # print("i...",i)
+    for j in range(i+1):
+        print(j+1, end=" ")
+        # print("j....",j)
+    print()
+
+#whaaaaat kai .....??
+
