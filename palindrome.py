e2e678710673486dd54bb7382af186e1ef91e38b
Mithelesh_1141 <48873398+dude1141@users.noreply.github.com>
2025-04-08 21:05:34 -0400

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+
+a='Mithlesh'
+c=""
+for i in range(len(a)-1,-1,-1):
+    c=c+a[i]
+    # print("c",c)
+
+a1='MADAM'
+a2=""
+for i in range(len(a1)-1,-1,-1):
+    a2 = a2+a1[i];
+    print('a2',a2)
+if(a1==a2):
+        print("palendrome")
+
+
+
