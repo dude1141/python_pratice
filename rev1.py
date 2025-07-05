d4b30a764b77808a9145a7341028d09f599af101
Mithelesh_1141 <48873398+dude1141@users.noreply.github.com>
2025-07-05 11:06:35 -0400

    Create rev1.py

---
+++ rev1.py
@@ -0,0 +1,15 @@
+a1=[100,129,999,1,2,1000]
+temp=a1[0]
+for i in range(0,len(a1)-1,1):
+    #print(a1[i+1])
+    if a1[i+1]>temp:
+        temp = a1[i+1]
+print(temp)
+
+a=2
+b=3
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
