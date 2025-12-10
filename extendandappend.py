d10103112526608ba53ce86a51c494b8e852738d
Mithelesh_1141 <48873398+dude1141@users.noreply.github.com>
2025-12-10 17:32:14 -0500

    Update extendandappend.py

--- extendandappend.py
+++ extendandappend.py
@@ -19,3 +19,23 @@ i    	type(i)is list?	      Action
 2        	 No             	append(2)
 [14, 15]	 Yes	            extend(flatten_list([14, 15])) → [14,15]
 7	         No             	append(7)
+
+
+i.. 1
+append []
+i.. 2
+append [1]
+i.. [14, 15]
+i.. 14
+append []
+i.. 15
+append [14]
+i.. 7
+append [1, 2, 14, 15]
+i.. 9
+append [1, 2, 14, 15, 7]
+i.. 18
+append [1, 2, 14, 15, 7, 9]
+i.. 10
+append [1, 2, 14, 15, 7, 9, 18]
+[1, 2, 14, 15, 7, 9, 18, 10]
