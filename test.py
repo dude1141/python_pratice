3add1e495f69e82f9c1805c53a148e7ddd6b2472
Mithelesh_1141 <48873398+dude1141@users.noreply.github.com>
2026-03-01 21:51:11 -0500

    Add string manipulation and email parsing examples

---
+++ test.py
@@ -0,0 +1,10 @@
+a="clound computing tel"
+print(a[-16:])
+
+print(a.replace("tel","sss",2))
+
+
+email="admin@gmail.com"
+print(email.find("@")+1)
+print(email[email.find("@")+1:])
+print(email[6:])
