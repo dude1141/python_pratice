cf49e638f3f005e34da685f1cc2ae38ba56f5408
Mithelesh_1141 <48873398+dude1141@users.noreply.github.com>
2026-03-12 23:05:23 -0400

    Refactor numberadd function and add lambda example
    Refactor number addition functions and add lambda example.

--- test.py
+++ test.py
@@ -8,3 +8,44 @@ email="admin@gmail.com"
 print(email.find("@")+1)
 print(email[email.find("@")+1:])
 print(email[6:])
+
+
+
+
+
+function and lambda:
+
+def numberadd(ival1,ival2):
+    ival1+ival2
+    return ival1+ival2
+
+
+rest=numberadd(5,6)
+print(rest)
+
+
+
+def numberadd(ival1,ival2):
+    ival1+ival2
+    return ival1+ival2
+
+
+ival1= int(input("Enter first value:"))
+ival2= int(input("Enter second value:"))
+rest=numberadd(ival1,ival2)
+print(rest)
+
+
+def numberadd(*vals):
+    #return ival1+ival2
+     return sum(vals)
+
+rest=0
+rest=numberadd(5,7,8)
+print(rest)
+
+
+rest=lambda x,y: x*y
+print("multplication of two numbers",rest(9,10))
+
+
