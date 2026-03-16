194d7ba407dca5fa529408ea96c12870675491fe
Mithelesh_1141 <48873398+dude1141@users.noreply.github.com>
2026-03-15 22:48:19 -0400

    Update test.py

--- test.py
+++ test.py
@@ -45,7 +45,107 @@ rest=numberadd(5,7,8)
 print(rest)
 
 
-rest=lambda x,y: x*y
-print("multplication of two numbers",rest(9,10))
 
+LISTS:
+
+
+
+Lists allow duplicates
+index based
+nested lists
+builtin methods like sort etc
+can store different data types
+ordered
+dynamic---append,  
+
+
+today notes:
+
+ival[2:4] start with 2 aand exclude 4
+
+istudent=[["venkat","8th",[1,2,3,5]],["venkasts","9th",[2,3,56,8]]]
+
+istudent=[["venkat","8th",[1,2,3,5]],["venkasts","9th",[2,3,56,8]]]
+
+print(istudent[1][2])
+
+
+
+ival1=[1,2,3,4]
+
+ival2=ival1
+
+ival2[2]=100
+print(ival1)
+print(ival2)
+
+
+if you change v2 it will change v1 list also
+
+if you id(ival1) and id(ival2) it has same memory location.
+
+
+
+if  you do ival2=ival1.copy()
+
+also do ival2[2]=1000
+then ival2 has different memory location
+
+shallow copy:
+
+ival1=[1,2,3,4]
+
+ival2=ival1.copy()
+
+ival2[2]=100
+print(ival1)
+print(ival2)
+
+
+
+
+
+ival1=[1,2,[30,40]]
+
+ival2=ival1.copy()
+ival2[1]=40
+ival2[2][0]=900
+print(ival1)
+print(ival2)
+
+
+
+
+#shallow copy inner list same memory location
+ival1=[1,2,[30,40]]
+
+ival2=ival1.copy()
+ival2[1]=40
+ival2[2][0]=900
+print(ival1)
+print(ival2)
+
+
+print(id(ival1))
+print(id(ival2))
+
+print(id(ival1[2]))
+print(id(ival2[2]))
+
+
+
+#deepcopy inner list has different location
+import copy
+
+ival3=[1,2,[50,60]]
+ival4=copy.deepcopy(ival3)
+
+ival3[1]=40
+ival3[2][0]=900
+
+print(id(ival3))
+print(id(ival4))
+
+print(id(ival3[2]))
+print(id(ival4[2]))
 
